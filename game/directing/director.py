3ef0be82208e1bfac1ae9fd08f7764bb75c00bec
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-10-21 18:09:07 -0500

    moved return fire to method, show enemy

--- game/directing/director.py
+++ game/directing/director.py
@@ -36,6 +36,7 @@ class Director:
         self._enemy_hit_scored = False
         self._enemy_destroyed = False
         self._defender_destroyed = False
+        self._ships_are_revealed = False
 
     # method holding game loop
     def start_game(self, cast):
@@ -72,8 +73,13 @@ class Director:
         velocity = self._keyboard_service.get_direction()
         cursor.set_velocity(velocity)   
         
-        # if game over, return
+        # if game over, set enemy ships to show, return
         if self._is_game_over:
+            if self._ships_are_revealed == False:
+                self._ships_are_revealed = True
+                ships = cast.get_actors("enemy_ships")
+                for i in range(len(ships)):
+                    ships[i].set_color(globals.RED_BOLD)
             return
 
         # check if enter key is down
@@ -90,15 +96,16 @@ class Director:
         
         Args:
             cast (Cast): The cast of actors.
+
+        Returns: 
+            nothing
         """
-        # set hit scored bool to false
+        # set hit scored, enemy hit, and damaged bools to false
         self._hit_scored = False
         self._enemy_hit_scored = False
-        self._damaged = False
 
         # get banners from cast for messages
         banner = cast.get_first_actor("banners")
-        banner_2 = cast.get_second_actor("banners")
 
         # get cursor from cast
         cursor = cast.get_first_actor("cursors")
@@ -115,10 +122,8 @@ class Director:
         # loop through enemy ships
         for ship in ships:
             
-            # check to see if the ship position equals the cursor position and the Enter key is pressed.
+            # check to see if the ship position equals the cursor position and the Enter key has been pressed.
             if self._enter_key_down and self._enter_key_up and ship.get_position().equals(cursor.get_position()):
-
-                # TODO UNCOMMENT WHEN DONE TROUBLESHOOTING 
                 
                 # show Enter key used
                 self._enter_key_down = False
@@ -129,54 +134,37 @@ class Director:
                 ship.set_text("X")
                 banner.set_text("Enemy ship hit!")
                 
-                # enemy fires back
-                return_fire = self._enemy_left(cast)
-                pre_volley = self._count_damage(cast)
-                for i in range(return_fire):
-                    self._enemy_hit_scored = ships[i].target()
-                post_volley = self._count_damage(cast)
-                if pre_volley > post_volley:
-                    banner_2.set_text("The enemy damaged our ships.")
-                else:
-                    banner_2.set_text("")
-
                 # set hit scored flag                    
                 self._hit_scored = True
 
-        if self._enter_key_down and self._enter_key_up and not self._hit_scored:
+                # enemy returns fire
+                self._return_fire(cast)
 
-            # TODO - UNCOMMENT WHEN DONE TROUBLESHOOTING 
+        # if the enter key been down and back up and player missed, do the following
+        if self._enter_key_down and self._enter_key_up and not self._hit_scored:
 
             # show Enter key as used
             self._enter_key_down = False
             self._enter_key_up = False  
             
-            # create new shot actor 
+            # create new shot actor and put in cast
             shot = Actor()
             shot.set_position(cursor.get_position())
             shot.set_text("X")
             shot.set_color(globals.WHITE)
             cast.add_actor("artillery", shot)
             banner.set_text("")
-            
-            # enemy fires back
-            return_fire = self._enemy_left(cast)
-            pre_volley = self._count_damage(cast)
-            for i in range(return_fire):
-                self._enemy_hit_scored = ships[i].target()
-            post_volley = self._count_damage(cast)
-            if pre_volley > post_volley:
-                banner_2.set_text("The enemy damaged our ships.")
-            else:
-                banner_2.set_text("")
 
-        # check to see if enemy is destroyed
+            # enemy returns fire
+            self._return_fire(cast)
+
+        # check to see if enemy is destroyed, set flag appropriately
         self._enemy_destroyed = True
         for ship in ships:
             if ship.get_text() != "X":
                 self._enemy_destroyed = False
         
-        # check to see if defenders are destroyed
+        # check to see if defenders are destroyed, set flag appropriately
         self._defender_destroyed = True
         for defender in defenses:
             if defender.get_text() != "X":
@@ -190,40 +178,73 @@ class Director:
                 win_message = "The enemy prevailed. You lose."
             banner.set_text(win_message)
             self._is_game_over = True
-       
-    # method to determine how many enemy ships can return fire
-    def _enemy_left(self, cast):
-        """Determins how many enemy ships are undamaged and returns
-        that number.
+    
+    # enemy returns fire method
+    def _return_fire(self, cast):
+        """Enemy returns fire up to 75% of working ships
 
-        Args: 
-            cast (Cast): the cast of actors
+        Args:
+            cast (Cast): assembly of all actors
 
         Returns:
-            count (int): integer of undamaged enemy ships
+            nothing        
         """
-        count = 0
-        enemies = cast.get_actors("enemy_ships")
-        for enemy in enemies:
-            if enemy.get_text() != "X":
-                count += 1
-        return count
-    
+        # get banners
+        # get needed cast members
+        ships = cast.get_actors("enemy_ships")
+        banner = cast.get_first_actor("banners")
+        banner_2 = cast.get_second_actor("banners")
+
+        # find out how many enemy ships are left
+        return_fire = self._count_ships(cast, "enemy_ships")
+        
+        # if 2 or more, number of return fire is 75% of working ships
+        if return_fire >= 2:
+            return_fire = int(return_fire * .75)
+
+        # see how many defense ships are left
+        pre_volley = self._count_ships(cast, "defense_ships")
+        
+        # conduct enemy return fire
+        for i in range(return_fire):
+            self._enemy_hit_scored = ships[i].target()
+        
+        # see how many defense ships are left and determine loss
+        post_volley = self._count_ships(cast, "defense_ships")
+        loss = pre_volley - post_volley
+        ships_left = use_word = ""
+        
+        # keep words used correct to numbers
+        if post_volley == 1:
+            ship_word = "ship"
+        else:
+            ship_word = "ships"
+        if loss == 1:
+            use_word = "ship"
+        else:
+            use_word = "ships"
+
+        # see how many enemy ships are working, and give report to player  
+        banner.set_text(f"Enemy ships left: {return_fire}")
+        banner_2.set_text(f"Damage Report: {loss} {use_word} damaged, {post_volley} {ship_word} left.")
+
     # method to count number of casualties
-    def _count_damage(self, cast):
-        """Gets the number of undamaged defense ships and returns it.
+    def _count_ships(self, cast, group):
+        """Gets the number of undamaged ships and returns it.
 
         Args:
             cast (Cast): cast of all actors
+            group (String): selection of cast members to get
 
         Returns:
             count (int): integer of how many defense ships remain
         """
-        count = 0
-        defenses = cast.get_actors("defense_ships")
-        for defense in defenses:
-            if defense.get_text() == "X":
-                count += 1
+        # get functioning defense forces from cast, count them out, return count 
+        forces = cast.get_actors(group)
+        count = len(forces)
+        for force in forces:
+            if force.get_text() == "X":
+                count -= 1
         return count
         
     # method to perform all outputs
